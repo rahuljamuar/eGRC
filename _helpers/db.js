a6763d2643248c025bb0485732c61e642799abf9
Rahul <rjamuar@mpart.in>
2022-02-21 19:29:00 +0530

    logger package

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,13 +1,15 @@
 const sql = require('mssql')
 const config = require('../config');
+const logger = require('./logger');
+
 
 const poolPromise = new sql.ConnectionPool(config.sql)
   .connect()
-  .then(pool => {
-    console.log('Connected to MSSQL')
+  .then(pool => {    
+    logger.info("Connected to MSSQL");
     return pool
   })
-  .catch(err => console.log('Database Connection Failed! Bad Config: ', err))
+  .catch(err => logger.error('Database Connection Failed! Bad Config: ', err))
 
 module.exports = {
   sql, poolPromise
--- _helpers/logger.js
+++ _helpers/logger.js
@@ -1,48 +1,48 @@
-// require('dotenv').config({ silent: true });
-// const { createLogger, transports, format } = require('winston');
+require('dotenv').config({ silent: true });
+const { createLogger, transports, format } = require('winston');
 
-// const logger = createLogger({
-//     transports: [
-//         new transports.Console({            
-//             level: 'info',
-//             format: format.combine(format.timestamp(), format.simple())
-//         }),
-//         new transports.File({
-//             filename: 'info.log',
-//             level: 'info',
-//             format: format.combine(format.timestamp(), format.json())
-//         }),
-//         // new transports.MongoDB({
-//         //     level: 'info',
-//         //     db: process.env.MONGODB_URI,
-//         //     metaKey:'meta',
-//         //     options: {
-//         //         useUnifiedTopology: true
-//         //     },
-//         //     collection: 'tms-log',
-//         //     format: format.combine(format.timestamp(), format.json())
-//         // }),
-//         new transports.Console({            
-//             level: 'error',
-//             format: format.combine(format.timestamp(), format.simple())
-//         }),
-//         new transports.File({
-//             filename: 'error.log',
-//             level: 'error',
-//             format: format.combine(format.timestamp(), format.json())
-//         }),
-//         // new transports.MongoDB({
-//         //     level: 'error',
-//         //     db: process.env.MONGODB_URI,
-//         //     metaKey:'meta',
-//         //     options: {
-//         //         useUnifiedTopology: true
-//         //     },
-//         //     collection: 'tms-log',
-//         //     format: format.combine(format.timestamp(), format.json())
-//         // }),
+const logger = createLogger({
+    transports: [
+        new transports.Console({            
+            level: 'info',
+            format: format.combine(format.timestamp(), format.simple())
+        }),
+        new transports.File({
+            filename: 'info.log',
+            level: 'info',
+            format: format.combine(format.timestamp(), format.json())
+        }),
+        // new transports.MongoDB({
+        //     level: 'info',
+        //     db: process.env.MONGODB_URI,
+        //     metaKey:'meta',
+        //     options: {
+        //         useUnifiedTopology: true
+        //     },
+        //     collection: 'tms-log',
+        //     format: format.combine(format.timestamp(), format.json())
+        // }),
+        new transports.Console({            
+            level: 'error',
+            format: format.combine(format.timestamp(), format.simple())
+        }),
+        new transports.File({
+            filename: 'error.log',
+            level: 'error',
+            format: format.combine(format.timestamp(), format.json())
+        }),
+        // new transports.MongoDB({
+        //     level: 'error',
+        //     db: process.env.MONGODB_URI,
+        //     metaKey:'meta',
+        //     options: {
+        //         useUnifiedTopology: true
+        //     },
+        //     collection: 'tms-log',
+        //     format: format.combine(format.timestamp(), format.json())
+        // }),
         
-//     ], meta: true
-// })
+    ], meta: true
+})
 
-// module.exports = logger;
+module.exports = logger;
