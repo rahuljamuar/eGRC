1c7e47773cb25fb0261249f101f902671429b69b
Rahul <rjamuar@mpart.in>
2022-08-08 22:12:06 +0530

    rename process location

--- _controllers/control_details.js
+++ _controllers/control_details.js
@@ -3,7 +3,7 @@
 const control_details_service = require('../_services/control_details');
 
 const getControlByControlAndPL = async (req, res, next) => {    
-    control_details_service.getControlByControlAndPL(req.headers.email, req.headers.token, req.query.control, req.query.process_location)
+    control_details_service.getControlByControlAndPL(req.headers.email, req.headers.token, req.query.control, req.query.performance_location)
         .then(control_details => control_details ? res.json(control_details) : res.sendStatus(404))
         .catch(err => next(err));
 }
